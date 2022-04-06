d730549d7d3cb1d1092d9edf7ac1d920801dd0e8
Antoll <antoine.ollivier@epita.fr>
2022-04-06 12:03:05 -0400

    ajout de inscriptionpage

--- app/src/main/java/uqac/dim/humadvisor/MainActivity.java
+++ app/src/main/java/uqac/dim/humadvisor/MainActivity.java
@@ -1,14 +1,25 @@
 package uqac.dim.humadvisor;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void inscriptionPage(View view)
+    {
+        setContentView(R.layout.inscription);
+    }
+
+    public void connexionPage(View view)
+    {
         setContentView(R.layout.activity_main);
     }
 }
